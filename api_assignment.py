5f988b1d3faac2448f26334a12d875131349d03d
ayushmatsoni <ayushmats@gmail.com>
2018-06-21 11:21:16 +0530

    Update api_assignment.py

--- api_assignment.py
+++ api_assignment.py
@@ -33,3 +33,17 @@ print(api.search("#india"))
 
 
 #question5
+import sendgrid
+import os
+from sendgrid.helpers.mail import *
+
+sg = sendgrid.SendGridAPIClient(apikey="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")
+from_email = Email("ayushmats@gmail.com")
+to_email = Email("richasood120@gmail.com")
+subject = "Sending with SendGrid is Fun"
+content = Content("text/plain", "and easy to do anywhere, even with Python")
+mail = Mail(from_email, subject, to_email, content)
+response = sg.client.mail.send.post(request_body=mail.get())
+print(response.status_code)
+print(response.body)
+print(response.headers)
