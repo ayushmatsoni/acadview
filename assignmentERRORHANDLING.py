eae6fc99e3dc51e6939923b46d2cb99c97b59b8d
ayushmatsoni <ayushmats@gmail.com>
2018-06-21 17:14:34 +0530

    Add files via upload

---
+++ assignmentERRORHANDLING.py
@@ -0,0 +1,61 @@
+#question1
+print("QUESTION 1")
+a=3
+try:
+    if a<4:
+        a=a/(a-3)
+        print(a)
+except ZeroDivisionError:
+    print("THIS IS ZERO DIVISION ERROR")
+
+print("")
+print("QUESTION2")
+#question2
+try:
+    l=[1,2,3]
+    print(l[3])
+except IndexError:
+    print("THIS IS INDEX ERROR")
+
+print("")
+print("QUESTION3")
+#question3
+print("'AN EXCEPTION'")
+
+print("")
+print("QUESTION4")
+#question4
+print(-5.0)
+print("a/b result is 0")
+
+print("")
+print("QUESTION5")
+#question5
+
+
+try :
+    import HELLO
+
+except ImportError:
+    print("THIS IS IMPORT ERROR")
+    pass
+
+try:
+    n=int(input("ENTER ANY NUMBER"))
+except ValueError:
+    print("THIS IS VALUE ERROR")
+    pass
+else:
+    print(n)
+try:
+    x=[1,2,3]
+    i=int(input("ENTER ANY INDEX"))
+    print(x[i])
+except IndexError:
+    print("THIS IS INDEX ERROR")
+
+
+print("")
+print("QUESTION6")
+#question6
+
